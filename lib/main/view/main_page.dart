35228ff28dc6edc975aa1e661cd5a4712f52fb9a
Yazeed AlKhalaf <yazeedfady@gmail.com>
2022-08-30 18:46:13 +0300

    change provider location in main

--- lib/main/view/main_page.dart
+++ lib/main/view/main_page.dart
@@ -1,5 +1,5 @@
 import 'package:alyamamah/l10n/l10n.dart';
-import 'package:alyamamah/main/view/provider/main_provider.dart';
+import 'package:alyamamah/main/provider/main_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 

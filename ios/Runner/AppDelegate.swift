b7211a004759ea5b0aff10a47f2e43661f796fbf
Yazeed AlKhalaf <yazeedfady@gmail.com>
2023-03-11 21:06:12 +0300

    add app indexing on launch first time

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,34 @@
 import UIKit
 import Flutter
+import CoreSpotlight
+import MobileCoreServices
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        GeneratedPluginRegistrant.register(with: self)
+        
+        if (!UserDefaults.standard.bool(forKey: "hasIndexedApp")) {
+            let searchableItem = CSSearchableItem(
+                uniqueIdentifier: "dev.alkhalaf.alyamamah",
+                domainIdentifier: "dev.alkhalaf.alyamamah",
+                attributeSet: createAttributeSet()
+            )
+            CSSearchableIndex.default().indexSearchableItems([searchableItem])
+        }
+        
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+    
+    func createAttributeSet() -> CSSearchableItemAttributeSet {
+        let attributeSet = CSSearchableItemAttributeSet(itemContentType: kUTTypeItem as String)
+        attributeSet.title = "Al Yamamah"
+        attributeSet.keywords = ["اليمامة", "alyamamah", "yamamah", "يمامة", "جامعة", "university", "app", "iOS"]
+        attributeSet.contentDescription = "An app for alyamamah university students."
+        
+        return attributeSet
+    }
 }

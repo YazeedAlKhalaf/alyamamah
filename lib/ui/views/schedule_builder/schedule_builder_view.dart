3e3a617f6068ef08c8cf3d537ece809e4db3c140
Yazeed AlKhalaf <yazeedfady@gmail.com>
2023-08-19 20:54:29 +0300

    enhance schedule builder

--- lib/ui/views/schedule_builder/schedule_builder_view.dart
+++ lib/ui/views/schedule_builder/schedule_builder_view.dart
@@ -1,3 +1,4 @@
+import 'package:alyamamah/core/constants.dart';
 import 'package:alyamamah/core/extensions/build_context.dart';
 import 'package:alyamamah/core/models/offered_course.dart';
 import 'package:alyamamah/core/router/yu_router.dart';
@@ -102,75 +103,118 @@ class _ScheduleBuilderViewState extends ConsumerState<ScheduleBuilderView> {
       scheduleBuilderViewModelProvider,
     );
 
+    final allCoursesHaveConflicts = scheduleBuilderViewState
+        .offeredCoursesSchedules
+        .every((schedule) => schedule.hasConflicts);
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          context.s.schedule_builder,
-          style: const TextStyle(
-            fontWeight: FontWeight.w500,
+        appBar: AppBar(
+          title: Text(
+            context.s.schedule_builder,
+            style: const TextStyle(
+              fontWeight: FontWeight.w500,
+            ),
           ),
         ),
-      ),
-      body: switch (scheduleBuilderViewState.status) {
-        ScheduleBuilderViewStatus.generating => const Center(
-            child: CircularProgressIndicator(),
-          ),
-        _ => PageView.builder(
-            onPageChanged: (int pageIndex) {
-              ref
-                  .read(scheduleBuilderViewModelProvider.notifier)
-                  .setSelectedScheduleIndex(pageIndex);
-            },
-            itemCount: scheduleBuilderViewState.offeredCoursesSchedules.length,
-            itemBuilder: (BuildContext context, int index) {
-              final offeredCoursesSchedule =
-                  scheduleBuilderViewState.offeredCoursesSchedules[index];
-
-              if (offeredCoursesSchedule.hasConflicts) {
-                return OfferedCoursesConflictsView(
-                  conflicts: offeredCoursesSchedule.conflicts,
-                  offeredCourses: widget.offeredCourses,
+        body: switch (scheduleBuilderViewState.status) {
+          ScheduleBuilderViewStatus.generating => const Center(
+              child: CircularProgressIndicator(),
+            ),
+          _ => () {
+              if (allCoursesHaveConflicts) {
+                return Padding(
+                  padding: const EdgeInsets.all(Constants.padding),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Text(
+                        '🤔',
+                        textAlign: TextAlign.center,
+                        style: context.textTheme.displayLarge,
+                      ),
+                      const SizedBox(height: Constants.spacing),
+                      Text(
+                        context.s
+                            .all_the_courses_that_you_have_selected_have_conflicts_please_remove_some_courses_and_try_again,
+                        textAlign: TextAlign.center,
+                        style: context.textTheme.titleMedium,
+                      ),
+                      const SizedBox(height: Constants.spacing),
+                      FilledButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Text(
+                          context.s.go_back,
+                        ),
+                      ),
+                    ],
+                  ),
                 );
               }
 
-              return CoursesSchedule(
-                scheduleDays: offeredCoursesSchedule.scheduleDays,
-                onCourseTap: (ScheduleEntry scheduleEntry) async {
-                  await ref
-                      .read(coursesViewModelProvider)
-                      .navigateToCourseDetails(scheduleEntry);
+              return PageView.builder(
+                onPageChanged: (int pageIndex) {
+                  ref
+                      .read(scheduleBuilderViewModelProvider.notifier)
+                      .setSelectedScheduleIndex(pageIndex);
                 },
-                onRefresh: () async {},
+                itemCount:
+                    scheduleBuilderViewState.offeredCoursesSchedules.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final offeredCoursesSchedule =
+                      scheduleBuilderViewState.offeredCoursesSchedules[index];
+
+                  if (offeredCoursesSchedule.hasConflicts) {
+                    return OfferedCoursesConflictsView(
+                      conflicts: offeredCoursesSchedule.conflicts,
+                      offeredCourses: widget.offeredCourses,
+                    );
+                  }
+
+                  return CoursesSchedule(
+                    scheduleDays: offeredCoursesSchedule.scheduleDays,
+                    onCourseTap: (ScheduleEntry scheduleEntry) async {
+                      await ref
+                          .read(coursesViewModelProvider)
+                          .navigateToCourseDetails(scheduleEntry);
+                    },
+                    onRefresh: () async {},
+                  );
+                },
               );
-            },
-          ),
-      },
-      bottomNavigationBar: BottomAppBar(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            FilledButton(
-              onPressed: switch (scheduleBuilderViewState.status) {
-                ScheduleBuilderViewStatus.submitting => null,
-                _ => () async {
-                    await ref
-                        .read(scheduleBuilderViewModelProvider.notifier)
-                        .submitSchedule();
-                  },
-              },
-              child: switch (scheduleBuilderViewState.status) {
-                ScheduleBuilderViewStatus.submitting => const ButtonLoading(),
-                ScheduleBuilderViewStatus.errorSubmitting => Text(
-                    context.s.please_try_again,
+            }()
+        },
+        bottomNavigationBar: switch (allCoursesHaveConflicts) {
+          true => null,
+          false => BottomAppBar(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  FilledButton(
+                    onPressed: switch (scheduleBuilderViewState.status) {
+                      ScheduleBuilderViewStatus.submitting => null,
+                      _ => () async {
+                          await ref
+                              .read(scheduleBuilderViewModelProvider.notifier)
+                              .submitSchedule();
+                        },
+                    },
+                    child: switch (scheduleBuilderViewState.status) {
+                      ScheduleBuilderViewStatus.submitting =>
+                        const ButtonLoading(),
+                      ScheduleBuilderViewStatus.errorSubmitting => Text(
+                          context.s.please_try_again,
+                        ),
+                      _ => Text(
+                          context.s.choose_schedule,
+                        ),
+                    },
                   ),
-                _ => Text(
-                    context.s.choose_schedule,
-                  ),
-              },
+                ],
+              ),
             ),
-          ],
-        ),
-      ),
-    );
+        });
   }
 }

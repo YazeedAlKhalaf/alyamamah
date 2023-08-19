877f8479bcc911f676fec9be84a8cb482ecd5699
Yazeed AlKhalaf <yazeedfady@gmail.com>
2023-08-19 16:22:36 +0300

    add the app features

---
+++ website/src/data/features.ts
@@ -0,0 +1,44 @@
+interface Feature {
+  title: string;
+  description: string;
+  icon: string;
+}
+
+export const features: Feature[] = [
+  {
+    title: "Personalized Schedules",
+    description:
+      "Easily craft and view your daily routine. No more missing appointments or forgetting tasks. Stay on track with clarity!",
+    icon: "🗓️",
+  },
+  {
+    title: "Absence Tracker",
+    description:
+      "Keep tabs on your absences. Be it for health, vacation, or any personal reason – we've got you covered.",
+    icon: "🚫",
+  },
+  {
+    title: "Grade Insights",
+    description:
+      "Monitor your academic performance. Access a detailed breakdown of your grades to see where you shine and where you can improve.",
+    icon: "📊",
+  },
+  {
+    title: "Profile Customization",
+    description:
+      "Update and personalize your profile. Reflect your style, preferences, and keep your information up-to-date with ease.",
+    icon: "🖼️",
+  },
+  {
+    title: "Home Screen Widgets",
+    description:
+      "Get a snapshot of your schedule directly on your home screen. No more tedious app openings. It’s all at your fingertips!",
+    icon: "📱",
+  },
+  {
+    title: "Theme Flexibility",
+    description:
+      "Switch between dark and light modes based on your preference. Whichever you choose, we ensure a delightful viewing experience.",
+    icon: "🌙",
+  },
+];

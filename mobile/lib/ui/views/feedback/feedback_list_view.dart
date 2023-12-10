724fc3f77700569e380ad7a62161c0c06d4607ba
Yazeed AlKhalaf <yazeedfady@gmail.com>
2023-12-10 22:44:52 +0300

    fix loading and refreshing stuff

--- mobile/lib/ui/views/feedback/feedback_list_view.dart
+++ mobile/lib/ui/views/feedback/feedback_list_view.dart
@@ -3,6 +3,7 @@ import 'package:alyamamah/core/extensions/build_context.dart';
 import 'package:alyamamah/core/router/yu_router.dart';
 import 'package:alyamamah/ui/views/feedback/feedback_state.dart';
 import 'package:alyamamah/ui/views/feedback/feedback_view_model.dart';
+import 'package:alyamamah/ui/widgets/error_view.dart';
 import 'package:alyamamah/ui/widgets/section_container.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
@@ -39,112 +40,120 @@ class _FeedbackListViewState extends ConsumerState<FeedbackListView> {
         FeedbackStatus.loadingFeedbacks => const Center(
             child: CircularProgressIndicator(),
           ),
-        FeedbackStatus.errorLoadingFeedbacks => Center(
-            child: Text(
-              context.s.error_loading_feedbacks,
-            ),
+        FeedbackStatus.errorLoadingFeedbacks => ErrorView(
+            title: context.s.something_went_wrong,
+            subtitle: context.s.error_loading_feedbacks,
+            onRefresh: () async {
+              await ref.read(feedbackViewModelProvider.notifier).getFeedbacks();
+            },
           ),
-        _ => CustomScrollView(
-            slivers: [
-              SliverToBoxAdapter(
-                child: SectionContainer(
-                  showTopPadding: false,
-                  children: [
-                    ListTile(
-                      leading: CircleAvatar(
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(999),
-                          child: Image.asset(
-                            'assets/images/student-council-logo.png',
-                          ),
-                        ),
-                      ),
-                      title: Text(
-                        context.s.create_feedback,
-                        style: context.textTheme.titleMedium?.copyWith(
-                          color: context.colorScheme.onSecondaryContainer,
-                        ),
-                      ),
-                      subtitle: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Expanded(
-                            child: Text(
-                              context.s.share_concerns_with_the_student_council,
-                              style: context.textTheme.bodyMedium,
-                            ),
-                          ),
-                        ],
-                      ),
-                      trailing: const Icon(Icons.chevron_right_rounded),
-                      onTap: () async {
-                        await ref
-                            .read(yuRouterProvider)
-                            .push(const FeedbackRoute());
-                      },
-                    ),
-                  ],
-                ),
-              ),
-              const SliverPadding(
-                padding: EdgeInsets.all(Constants.padding),
-              ),
-              SliverList.separated(
-                itemCount: feedbackState.feedbacks.length,
-                itemBuilder: (context, index) {
-                  final feedback = feedbackState.feedbacks[index];
-
-                  final locale = Localizations.localeOf(context).languageCode;
-                  final createdAtAsDateTime = feedback.createdAt.toDateTime();
-                  final createdAtDay = intl.DateFormat.EEEE(locale).format(
-                    createdAtAsDateTime,
-                  );
-                  final createdAtDate = intl.DateFormat.yMMMMd(locale).format(
-                    createdAtAsDateTime,
-                  );
-
-                  return SectionContainer(
+        _ => RefreshIndicator(
+            onRefresh: () async {
+              await ref.read(feedbackViewModelProvider.notifier).getFeedbacks();
+            },
+            child: CustomScrollView(
+              slivers: [
+                SliverToBoxAdapter(
+                  child: SectionContainer(
                     showTopPadding: false,
                     children: [
                       ListTile(
+                        leading: CircleAvatar(
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(999),
+                            child: Image.asset(
+                              'assets/images/student-council-logo.png',
+                            ),
+                          ),
+                        ),
                         title: Text(
-                          feedback.title,
+                          context.s.create_feedback,
                           style: context.textTheme.titleMedium?.copyWith(
                             color: context.colorScheme.onSecondaryContainer,
                           ),
                         ),
-                        subtitle: Column(
+                        subtitle: Row(
                           mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Text(
-                              context.isArabic
-                                  ? feedback.category.nameAr
-                                  : feedback.category.nameEn,
-                              style: context.textTheme.bodySmall,
-                            ),
-                            const SizedBox(height: Constants.spacing),
-                            Text(
-                              feedback.body,
-                              style: context.textTheme.bodyMedium,
+                            Expanded(
+                              child: Text(
+                                context
+                                    .s.share_concerns_with_the_student_council,
+                                style: context.textTheme.bodyMedium,
+                              ),
                             ),
                           ],
                         ),
-                        trailing: Text(
-                          '$createdAtDay\n$createdAtDate',
-                          style: context.textTheme.bodySmall?.copyWith(
-                            color: context.colorScheme.onSecondaryContainer,
-                          ),
-                        ),
+                        trailing: const Icon(Icons.chevron_right_rounded),
+                        onTap: () async {
+                          await ref
+                              .read(yuRouterProvider)
+                              .push(const FeedbackRoute());
+                        },
                       ),
                     ],
-                  );
-                },
-                separatorBuilder: (context, index) {
-                  return const SizedBox(height: Constants.padding);
-                },
-              ),
-            ],
+                  ),
+                ),
+                const SliverPadding(
+                  padding: EdgeInsets.all(Constants.padding),
+                ),
+                SliverList.separated(
+                  itemCount: feedbackState.feedbacks.length,
+                  itemBuilder: (context, index) {
+                    final feedback = feedbackState.feedbacks[index];
+
+                    final locale = Localizations.localeOf(context).languageCode;
+                    final createdAtAsDateTime = feedback.createdAt.toDateTime();
+                    final createdAtDay = intl.DateFormat.EEEE(locale).format(
+                      createdAtAsDateTime,
+                    );
+                    final createdAtDate = intl.DateFormat.yMMMMd(locale).format(
+                      createdAtAsDateTime,
+                    );
+
+                    return SectionContainer(
+                      showTopPadding: false,
+                      children: [
+                        ListTile(
+                          title: Text(
+                            feedback.title,
+                            style: context.textTheme.titleMedium?.copyWith(
+                              color: context.colorScheme.onSecondaryContainer,
+                            ),
+                          ),
+                          subtitle: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                context.isArabic
+                                    ? feedback.category.nameAr
+                                    : feedback.category.nameEn,
+                                style: context.textTheme.bodySmall,
+                              ),
+                              const SizedBox(height: Constants.spacing),
+                              Text(
+                                feedback.body,
+                                style: context.textTheme.bodyMedium,
+                              ),
+                            ],
+                          ),
+                          trailing: Text(
+                            '$createdAtDay\n$createdAtDate',
+                            style: context.textTheme.bodySmall?.copyWith(
+                              color: context.colorScheme.onSecondaryContainer,
+                            ),
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                  separatorBuilder: (context, index) {
+                    return const SizedBox(height: Constants.padding);
+                  },
+                ),
+              ],
+            ),
           ),
       },
     );

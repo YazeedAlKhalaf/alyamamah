0cb926570bc9ed735dbb515bc26ab0ca34642151
Yazeed AlKhalaf <yazeedfady@gmail.com>
2023-12-15 21:02:47 +0300

    fix for query
    this is what hapens when you deploy to production without testing locally first

--- quasar/pkg/feedback/store/queries/feedback_status_history.sql
+++ quasar/pkg/feedback/store/queries/feedback_status_history.sql
@@ -1,5 +1,5 @@
 -- name: ListFeedbackStatusHistoryByFeedbackId :many
 SELECT fs.id, fs.name_ar, fs.name_en, fsh.created_at FROM feedback_status_history AS fsh
-JOIN feedback_status AS fs ON fsh.feedback_status_id = fs.id
+JOIN feedback_status AS fs ON fsh.status_id = fs.id
 WHERE fsh.feedback_id = $1
 ORDER BY created_at DESC;
--- quasar/pkg/feedback/store/sqlc/feedback_status_history.sql.go
+++ quasar/pkg/feedback/store/sqlc/feedback_status_history.sql.go
@@ -15,7 +15,7 @@ import (
 const listFeedbackStatusHistoryByFeedbackId = `-- name: ListFeedbackStatusHistoryByFeedbackId :many
 SELECT fs.id, fs.name_ar, fs.name_en, fsh.created_at FROM feedback_status_history AS fsh
 JOIN feedback_status AS fs ON fsh.feedback_status_id = fs.id
-WHERE fsh.feedback_id = $1
+WHERE feedback_id = $1
 ORDER BY created_at DESC
 `
 

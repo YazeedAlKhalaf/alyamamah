edac245d76bad12a7c4ce5b23527f684772b0a9f
Yazeed AlKhalaf <yazeedfady@gmail.com>
2024-01-08 02:44:26 +0300

    add author credits to splash screen

--- mobile/lib/ui/views/startup/startup_view.dart
+++ mobile/lib/ui/views/startup/startup_view.dart
@@ -1,3 +1,5 @@
+import 'package:alyamamah/core/constants.dart';
+import 'package:alyamamah/core/extensions/build_context.dart';
 import 'package:alyamamah/ui/views/startup/startup_view_model.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
@@ -21,10 +23,46 @@ class _StartupViewState extends ConsumerState<StartupView> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: CircularProgressIndicator(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Spacer(),
+            Container(
+              clipBehavior: Clip.hardEdge,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+              ),
+              child: Image.asset(
+                'assets/logo/logo.png',
+                height: 200,
+              ),
+            ),
+            const SizedBox(height: Constants.padding * 3),
+            const Center(child: CircularProgressIndicator()),
+            const Spacer(),
+            Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.secondaryContainer,
+                borderRadius: BorderRadius.circular(999),
+              ),
+              padding: const EdgeInsets.symmetric(
+                horizontal: Constants.padding,
+                vertical: Constants.padding / 2,
+              ),
+              child: Text(
+                context.s.made_with_love_by_yazeed_alkhalaf,
+                style: context.textTheme.titleMedium?.copyWith(
+                  color: Theme.of(context).colorScheme.onSecondaryContainer,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            const SizedBox(height: Constants.padding * 2),
+          ],
         ),
       ),
     );
